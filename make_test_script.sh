7022c20e37e6159d954ddcacaef0ad14ceb28e2e
CavendishBanana <155924297+CavendishBanana@users.noreply.github.com>
2024-03-31 18:41:20 +0200

    Update script to be ran on jenkins image

--- make_test_script.sh
+++ make_test_script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-manage_file_dir=$(cat /home/task/manage_file_dir_path.txt)
+manage_file_dir=$(cat /home/jenkins/task/manage_file_dir_path.txt)
 if [[ $manage_file_dir == "" ]] ;then
-  cd "/home/task/aplikacja_lista_zakopow/lista_zakopow"
+  cd "/home/jenkins/task/aplikacja_lista_zakopow/lista_zakopow"
 else
   cd "$manage_file_dir"
 fi
 test_string="test"
-python3 manage.py $test_string > /home/task/test_report.txt
-cd /home/task/
+python3 manage.py $test_string > /home/jenkins/task/test_report.txt
+cd /home/jenkins/task/
 test_failed=$(cat test_report.txt | tail -n 2 | grep FAILED | wc -l)
 
 if [[ $test_failed -gt 0 ]] ;then
   echo "Error detected - check report for details"
-  cat /home/task/test_report.txt
+  cat /home/jenkins/task/test_report.txt
   declare -i one=1 zero=0 infinity=one/zero
 fi
